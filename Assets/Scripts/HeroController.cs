38f535b23d7a131612ce692c4219689767c60d85
hellfreezes <hellfreezes@yandex.ru>
2018-01-16 23:02:46 +0700

    02-0116

---
+++ Assets/Scripts/HeroController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour {
+    [SerializeField]
+    private float movementSpeed = 1.0f;
+
+
+    private Rigidbody2D heroRigidbody;
+
+	// Use this for initialization
+	void Start () {
+        heroRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        MoveHandle(horizontal);
+	}
+
+    private void MoveHandle(float horizontal)
+    {
+        heroRigidbody.velocity = new Vector2(horizontal * movementSpeed, heroRigidbody.velocity.y);
+    }
+}
