808cfc500168ccdabb88b02cfab4b51a2efea21c
hellfreezes <hellfreezes@yandex.ru>
2018-01-17 15:42:46 +0700

    0117-01

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -6,12 +6,16 @@ public class HeroController : MonoBehaviour {
     [SerializeField]
     private float movementSpeed = 1.0f;
 
+    private bool facingRight;
 
     private Rigidbody2D heroRigidbody;
+    private Animator heroAnimator;
 
 	// Use this for initialization
 	void Start () {
+        facingRight = true;
         heroRigidbody = GetComponent<Rigidbody2D>();
+        heroAnimator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -19,10 +23,24 @@ public class HeroController : MonoBehaviour {
         float horizontal = Input.GetAxis("Horizontal");
 
         MoveHandle(horizontal);
+        Flip(horizontal);
 	}
 
     private void MoveHandle(float horizontal)
     {
         heroRigidbody.velocity = new Vector2(horizontal * movementSpeed, heroRigidbody.velocity.y);
+        heroAnimator.SetFloat("speed", Mathf.Abs(horizontal));
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
     }
 }
